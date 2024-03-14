36c3abd7de47a9610ee051754248cf1d75b6bd2d
Jonhy <joao-morrudo@hotmail.com.br>
2024-03-14 17:29:18 -0300

    ex003

---
+++ ex003/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos</title>
+</head>
+<body>
+    
+    <h1>Tipos de Dados</h1>
+
+    <?php 
+    
+        $num = 0;
+        $val = 3.5;
+        $nome = "Nome";
+        $verdade = false;
+        const VALOR = 3;
+
+        echo "O valor é $num e o número é " . VALOR;
+
+    ?>
+
+</body>
+</html>
